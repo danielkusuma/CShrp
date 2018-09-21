d9f50ffbb27dca120a6cdb74c909ba0f584d2492
Daniel Kusuma <d@nielengineer.com>
2018-09-22 06:09:06 +1000

    commit

--- ExConsoleApp/ExConsoleApp/Program.cs
+++ ExConsoleApp/ExConsoleApp/Program.cs
@@ -8,7 +8,22 @@ using System.IO;
 namespace ExConsoleApp {
     class Program {
         static void Main(string[] args) {
-            File.OpenWrite
+            /*
+            // File and FileInfo
+            var path = @"c:\somefile.jpg";
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
+            File.Delete(path);
+            if( File.Exists(path) ) {
+
+            }
+            var content = File.ReadAllText(path);
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+            if( fileInfo.Exists ) {
+
+            }
+            */
             /*
             // Demo Codility
             // create an array of N (integer between 1 - 100000)
